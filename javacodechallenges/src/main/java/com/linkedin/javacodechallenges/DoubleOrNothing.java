a5d4a4e87c630195d9eb50dd2ad6cdb88f495b7c
smoser-LiL <smoser-LiL@localhost>
2022-11-08 22:48:59 +0000

    Moving files  using main

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,35 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
   public void playGame() {
-    // TODO: Implement method
+
+    Random random = new Random();
+    int win = 10;
+    boolean cont = true;
+    Scanner scanner = new Scanner(System.in);
+    while (cont){
+    System.out.println("Do you want to play ? yes / no ");
+    String str = scanner.nextLine();
+    if (str.equals("no"))
+    {
+      System.out.println("your cuurent win is " + win);
+      break;
+    }
+    if (str.equals("yes"))
+    {
+    if (random.nextInt(win) % 2 == 0){
+      win *= 2;
+      System.out.println("your cuurent win is " + win);
+    }
+    else {
+      System.out.println("KO!!! you lost");
+      break;
+    }
+  }
+}
   }
 }
