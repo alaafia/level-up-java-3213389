3d991d9843feb09ca5e050e246865a0597b48f89
aicha laafia <54075891+alaafia@users.noreply.github.com>
2022-12-26 16:28:07 +0000

    solved

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,24 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to introduce yourself ?");
+        String res = scanner.nextLine();
+        if (res.equals("yes")){
+            System.out.println("Type your first name .");
+            String first = scanner.nextLine();
+            System.out.println("Type your last name .");
+            String last = scanner.nextLine();
+            System.out.println("Type your age  .");
+            int age = scanner.nextInt();
+
+            Person person = new Person(first, last, age);
+            person.introduce();
+        }
     }
 }
